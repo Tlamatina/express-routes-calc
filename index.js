364b8cb3a49598accf61fb5e1ace9f36ebc09984
Tlamatina <Tassi.lamatina@gmail.com>
2021-05-27 15:46:46 -0700

    git commit with bonus answer

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.send('hello world!👋')
 })
-
 app.get('/add/:x/:y', (req, res) => {
     const x = parseInt(req.params.x)
     const y = parseInt(req.params.y)
@@ -33,6 +32,29 @@ app.get("/*", (req, res) => {
     const x = parseInt(req.params.x)
     const y = parseInt(req.params.y)
     res.send(`${req.params}`)
+  })
+
+  //bonus solution:
+  app.get("/*", (req, res) => {
+    let math = req.query.math
+    let nums = req.params[0].split('/')
+    nums = nums.map(num => parseInt(num))
+
+
+    switch(math) {
+        case 'add':
+            res.send(`${nums.reduce((a, b) => a + b)}`)
+            break
+        case 'subtract': 
+            res.send(`${nums.reduce((a, b) => a - b)}`])
+            break
+        case 'multiply':
+            res.send(`${nums.reduce((a, b) => a * b)}`)
+            break
+        case 'divide':
+                res.send(`${nums.reduce((a, b) => a / b)}`)
+                break
+    }
   })
 
 app.listen(PORT, () => {
